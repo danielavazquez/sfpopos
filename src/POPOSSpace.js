212c9bb39907dade42cb722f2be4b612cdf4e994
daniela <daniela_056@hotmail.com>
2021-10-09 17:22:29 -0600

    Content component built

--- src/POPOSSpace.js
+++ src/POPOSSpace.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import logo from './logo.svg'
 
-function POPOSSpace(){
-  return(
+function POPOSSpace(props) {
+  const { name, image, address } = props
+  return (
     <div>
-      <img src={logo} width="300" height="300" alt="Hello" />
-      <h1>Name...</h1>
-      <div>Address...</div>
+      <img
+        src={`${process.env.PUBLIC_URL}images/${image}`}
+        width="300"
+        height="300"
+        alt="Hello"
+      />
+      <h1>{name}</h1>
+      <div>{address}</div>
     </div>
   )
 }
