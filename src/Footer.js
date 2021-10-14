169a4bc0cd8238219d065f7c4519c60188cb7f98
daniela <daniela_056@hotmail.com>
2021-10-13 20:00:59 -0600

    Footer component

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.css';
+
+function Footer() {
+  return(
+  <div className="Footer">
+    <div>Daniela Vazquez copyright 2021</div>
+  </div>
+  )
+}
+
+export default Footer
